e3fa1a55c81b2fc2b8c5406f17f6bb2fdeeb2745
simon <sdevissc@cern.ch>
2015-01-12 11:45:11 +0100

    second commit

---
+++ RecoToGenTreeFiller.py
@@ -0,0 +1,75 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        '/store/caf/user/sdevissc/TTbar_test1.root',
+	'/store/caf/user/sdevissc/TTbar_test2.root',
+	'/store/caf/user/sdevissc/TTbar_test3.root',
+	'/store/caf/user/sdevissc/TTbar_test4.root'
+    )
+)
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOEventContent.outputCommands,
+    fileName = cms.untracked.string('file:out_inRECO.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string('RECOfromRECO'),
+        dataTier = cms.untracked.string('AODSIM')
+    )
+)
+
+
+process.demo = cms.EDAnalyzer('ElectronID',
+ genParticleTag=cms.InputTag("genParticles"),
+ gedgsfElectronTag= cms.InputTag("gedGsfElectrons"),
+ GenPTag=cms.InputTag("genParticles"),
+ JetsTag=cms.InputTag("ak5PFJets"),
+ PVerTag=cms.InputTag("offlinePrimaryVertices"),
+ simtracksTag = cms.InputTag("g4SimHits"),
+ tracksTag = cms.InputTag("electronGsfTracks"),
+ TrackingParticleTag=cms.InputTag("mix","MergedTrackTruth"),
+ conversionTag=cms.InputTag("allConversions"),
+ beamSpotTag=cms.InputTag("offlineBeamSpot"),
+ maximumLongitudinalImpactParameter= cms.double(8),
+ minHits = cms.uint32(8),
+ UseRECO=cms.bool(True),
+ Verbose=cms.bool(True)
+
+)
+
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'MCRUN2_72_V3', '')
+
+
+import SimTracker.TrackAssociation.quickTrackAssociatorByHits_cfi
+from SimTracker.TrackerHitAssociation.clusterTpAssociationProducer_cfi import *
+process.load("SimTracker.TrackAssociation.quickTrackAssociatorByHits_cfi")
+process.quickTrackAssociatorByHits.useClusterTPAssociation = cms.bool(True)
+process.load("SimTracker.TrackerHitAssociation.clusterTpAssociationProducer_cfi")
+
+process.p = cms.Path(process.demo)
+process.schedule = cms.Schedule(process.p)
+
